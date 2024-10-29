fb45438c8c9858254d284264d50d7b9818ce6d22
foggyww <2274878949@qq.com>
2024-10-30 00:24:01 +0800

    [feat] 完善Bill的数据库操作

--- app/src/main/java/com/example/hustbill/db/BillHelper.kt
+++ app/src/main/java/com/example/hustbill/db/BillHelper.kt
@@ -1,8 +1,14 @@
 package com.example.hustbill.db
 
 import com.example.hustbill.config.BillType
+import com.example.hustbill.config.Config
+import com.example.hustbill.config.toBillType
 import com.example.hustbill.utils.Date
+import com.example.hustbill.utils.SP
+import com.example.hustbill.utils.toDate
+import com.example.hustbill.utils.toInt
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.withContext
 
 data class Bill(
@@ -22,8 +28,120 @@ object BillHelper {
     ) {
         withContext(Dispatchers.IO) {
             try {
+                getAppDatabase().billDao().insertBill(
+                    BaseBill(
+                        bill.name,
+                        bill.msg,
+                        bill.type.cnName,
+                        bill.amount,
+                        bill.date.toInt,
+                        Config.bookId,
+                        bill.source
+                    )
+                )
                 ioCallback.onCompleted(Unit)
             } catch (t: Throwable) {
+                ioCallback.onErrorHandler(t)
+            }
+        }
+    }
+
+    suspend fun updateBill(
+        bill: Bill,
+        ioCallback: IOCallback<Unit>,
+    ){
+        withContext(Dispatchers.IO) {
+            try {
+                getAppDatabase().billDao().insertBill(
+                    BaseBill(
+                        bill.name,
+                        bill.msg,
+                        bill.type.cnName,
+                        bill.amount,
+                        bill.date.toInt,
+                        Config.bookId,
+                        bill.source
+                    )
+                )
+                ioCallback.onCompleted(Unit)
+            } catch (t: Throwable) {
+                ioCallback.onErrorHandler(t)
+            }
+        }
+    }
+
+    suspend fun deleteBill(
+        bill: Bill,
+        ioCallback: IOCallback<Unit>,
+    ){
+        withContext(Dispatchers.IO) {
+            try {
+                getAppDatabase().billDao().deleteBill(
+                    BaseBill(
+                        bill.name,
+                        bill.msg,
+                        bill.type.cnName,
+                        bill.amount,
+                        bill.date.toInt,
+                        Config.bookId,
+                        bill.source
+                    )
+                )
+                ioCallback.onCompleted(Unit)
+            } catch (t: Throwable) {
+                ioCallback.onErrorHandler(t)
+            }
+        }
+    }
+
+    suspend fun collectBillList(ioCallback: IOCallback<MFlow<List<Bill>>>){
+        withContext(Dispatchers.IO){
+            try {
+                val flow = getAppDatabase().billDao().collectBillByDate(
+                    Config.bookId,
+                    Config.monthRange.start.toInt,
+                    Config.monthRange.end.toInt
+                )
+                val mFlow =  MFlow(
+                    flow.map {
+                        it.map { bBill->
+                            Bill(
+                                bBill.name,
+                                bBill.msg,
+                                bBill.type.toBillType!!,
+                                bBill.amount,
+                                bBill.date.toDate,
+                                bBill.source
+                            )
+                        }
+                    }
+                )
+                ioCallback.onCompleted(mFlow)
+            }catch (t:Throwable){
+                ioCallback.onErrorHandler(t)
+            }
+        }
+    }
+
+    suspend fun queryBillList(ioCallback: IOCallback<List<Bill>>){
+        withContext(Dispatchers.IO){
+            try {
+                val r = getAppDatabase().billDao().queryBillByDate(
+                    Config.bookId,
+                    Config.monthRange.start.toInt,
+                    Config.monthRange.end.toInt
+                ).map {
+                    Bill(
+                        it.name,
+                        it.msg,
+                        it.type.toBillType!!,
+                        it.amount,
+                        it.date.toDate,
+                        it.source
+                    )
+                }
+                ioCallback.onCompleted(r)
+            }catch (t:Throwable){
                 ioCallback.onErrorHandler(t)
             }
         }
